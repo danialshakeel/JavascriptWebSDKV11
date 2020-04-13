6d68dc2c15b1326cedf8ad340a9f83e38f4a446f
malik9353 <malikmousa9353@gmail.com>
2020-04-13 20:23:55 +0500

    Server Added

---
+++ ReactApp/build/precache-manifest.8c43faea1a4a3cb4cf1014fff94b36f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c09f40098671d48c35477a5e490c8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b2b3735a7b85ba0f0c",
+    "url": "/static/css/main.cdf77f43.chunk.css"
+  },
+  {
+    "revision": "b493937ed5dfa2d80739",
+    "url": "/static/js/2.a3021e2a.chunk.js"
+  },
+  {
+    "revision": "51b2b3735a7b85ba0f0c",
+    "url": "/static/js/main.f53d6217.chunk.js"
+  },
+  {
+    "revision": "3dd52d6ac62a7d6c3122",
+    "url": "/static/js/runtime-main.8a0b6508.js"
+  },
+  {
+    "revision": "c6c0d9529f2ed5b233fc50de1c57feea",
+    "url": "/static/media/acuant-logo@2x@3x.c6c0d952.png"
+  },
+  {
+    "revision": "0c7248855fa32cf64ed57349a6fb6df4",
+    "url": "/static/media/card_back@3x.0c724885.png"
+  },
+  {
+    "revision": "d268a33dd7020770c9be64f14571759a",
+    "url": "/static/media/illustration1@3x.d268a33d.png"
+  },
+  {
+    "revision": "406cb2b985fcbd5dca1111b268798326",
+    "url": "/static/media/illustration2@3x.406cb2b9.png"
+  },
+  {
+    "revision": "5e4db5272f8a48d5505821f0fb762833",
+    "url": "/static/media/image_correct.5e4db527.jpg"
+  },
+  {
+    "revision": "0b1f3e62b46ab75ae3ea73edf8f65063",
+    "url": "/static/media/scan_process.0b1f3e62.gif"
+  }
+]);
